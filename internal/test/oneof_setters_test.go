224e74ca84db871d7f969988597a263bc647a2f4
James Harris <contact@jamesharris.id.au>
2023-08-04 14:06:23 +1000

    Add tests for one-of setters.

---
+++ internal/test/oneof_setters_test.go
@@ -0,0 +1,60 @@
+package test_test
+
+import (
+	"testing"
+
+	. "github.com/dogmatiq/primo/internal/test"
+	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func TestOneOfSetters(t *testing.T) {
+	m := &Container{}
+
+	{
+		v := &Value{}
+		m.SetFieldA1(v)
+		if m.GetFieldA1() != v {
+			t.Fatal("SetFieldA1() did not set the field")
+		}
+	}
+
+	{
+		v := &Value{}
+		m.SetFieldA2(v)
+		if m.GetFieldA2() != v {
+			t.Fatal("SetFieldA2() did not set the field")
+		}
+	}
+
+	{
+		v := "<string>"
+		m.SetFieldA3(v)
+		if m.GetFieldA3() != v {
+			t.Fatal("SetFieldA3() did not set the field")
+		}
+	}
+
+	{
+		v := &timestamppb.Timestamp{}
+		m.SetFieldB1(v)
+		if m.GetFieldB1() != v {
+			t.Fatal("SetFieldB1() did not set the field")
+		}
+	}
+
+	{
+		v := int32(123)
+		m.SetFieldB2(v)
+		if m.GetFieldB2() != v {
+			t.Fatal("SetFieldB2() did not set the field")
+		}
+	}
+
+	{
+		v := Enumeration(123)
+		m.SetFieldB3(v)
+		if m.GetFieldB3() != v {
+			t.Fatal("SetFieldB3() did not set the field")
+		}
+	}
+}
